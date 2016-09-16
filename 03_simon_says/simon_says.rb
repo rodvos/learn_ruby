a975d21c16c2d3a8b373f908a5c0ff6e4f13d04f
rodvos <robinraposa@gmail.com>
2016-09-16 21:10:31 +0300

    simon's ex finished

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
 #write your code here
+def echo(greet)
+	greet	
+end
+
+def shout(roep)
+	roep.upcase
+end
+
+def repeat *args
+	unless args.length == 2
+		args[0] + " " + args[0]
+	else
+		(args[0] + " ") * ( args[1] -1) + args[0]
+	end
+end
+
+def start_of_word(word, letter)
+	word[0, letter]
+end
+
+def first_word(word)
+	word.split[0]
+end
+
+def titleize(word)
+	sentence = word.split
+	sentence.each do |x|
+		unless x == 'and' || x == 'over' || (x == 'the' && sentence[0] != 'the')
+		x.capitalize!
+	end
+	end
+	sentence.join(" ")
+end
