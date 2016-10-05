d0f7baafd6da80d98d79b886ac27779f12a3f03b
rodvos <robinraposa@gmail.com>
2016-10-05 19:22:45 +0300

    finished 5-6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
 # write your code here
+	attr_accessor :title
+def title=(book_title)
+	except = ["and","the","in","of","a","an"]
+	words= book_title.split
+	words.each do |word|
+		if except.include?(word)
+		else
+			word.capitalize!
+		end
+	words[0].capitalize!
 end
+@title = words.join(" ")
+end
+
+end
+
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :time_string
+	def initialize
+		@seconds = 0
+	end
+	def time_string
+	hours = @seconds / 3600
+	minutes = (@seconds % 3600) / 60
+	@seconds = (@seconds % (3600)) % 60
+
+	padded(hours) +":" + padded(minutes) +":" + padded(@seconds)
+
+	end
+	def padded(s)
+		if s.to_s.length < 2
+			return "0"+ s.to_s
+		else
+			return s.to_s
+	end
+end
 end
